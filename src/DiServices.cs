a99ae6c6f1fef8acdc2ebc5142d93922f4810034
Dubzer <me@dubzer.dev>
2025-11-26 18:01:19 +0300

    Update to .NET 10

--- src/DiServices.cs
+++ src/DiServices.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Telegram.Bot;
+using Telegram.Bot.AspNetCore;
 using TgTranslator.Data;
 using TgTranslator.Data.DTO;
 using TgTranslator.Data.Options;
